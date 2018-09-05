5c0ee560a5e74679a68dc2a71fe550f9da019026
kiryl_zayets <kiril.zayets@gmail.com>
2018-09-05 16:10:22 -0400

    SpiralOrder added.

---
+++ src/main/java/problems/array/SpiralOrder.java
@@ -0,0 +1,84 @@
+package problems.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kiryl_zayets on 9/5/18.
+ */
+public class SpiralOrder {
+
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if (matrix.length == 0) return res;
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int size = row * col;
+
+        int repeat = (int) Math.ceil((double) (row * col) / (row+col));
+
+        int i = 0;
+        int j = 0;
+        int k = col - 1;
+        int h = row - 1;
+        int total = 0;
+
+        for (int l = 0; l < repeat; l++) {
+
+            while (i < col & total != size) {
+                res.add(matrix[l][i++]);
+                total++;
+            }
+
+            i = l+1;
+
+            if (total == size) break;
+
+            j = l+1;
+            while (j < row & total != size) {
+                res.add(matrix[j++][col - 1]);
+                total++;
+            }
+            j = i+1;
+            if (total == size) break;
+
+            while (k > l) {
+                res.add(matrix[row - 1][k - 1]);
+                k--;
+                total++;
+            }
+
+            while (h > l + 1) {
+                res.add(matrix[h - 1][l]);
+                h--;
+                total++;
+            }
+
+            row = row - 1;
+            col = col - 1;
+            k = col - 1;
+            h = row - 1;
+        }
+
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[][] matrx = new int[5][5];
+        int c = 1;
+        for (int i = 0; i < matrx.length; i++) {
+
+            for (int j = 0; j < matrx[0].length; j++) {
+                matrx[i][j] = c;
+                c++;
+            }
+        }
+
+        ArrayList res = (ArrayList) SpiralOrder.spiralOrder(matrx);
+        System.out.println(res);
+    }
+}
